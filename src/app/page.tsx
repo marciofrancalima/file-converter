d875a30d0684836a06b3e4777c6b534ae39d8137
Márcio França Lima <marciofrancalima@hotmail.com>
2023-10-12 11:31:18 -0300

    chore: initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@/components/ui/button'
+export default function Home() {
+  return (
+    <div>
+      <h1>Seja bem vindo ao Conversor de arquivos</h1>
+      <Button variant="secondary">OK</Button>
+    </div>
+  )
+}
