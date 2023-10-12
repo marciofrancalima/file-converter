e9db60e7db54815e96559d43697d43c8478ab1be
Márcio França Lima <marciofrancalima@hotmail.com>
2023-10-12 19:24:37 -0300

    feat: add content to privacy-policy page

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -1,5 +1,135 @@
 const PrivacyPolicy = () => {
-  return <h2>Termos e Política de privacidade</h2>
+  return (
+    <div className="text-md mb:pb-8 space-y-12 pb-4 text-gray-500 md:text-lg">
+      <p>Data da última revisão: 12/10/2023</p>
+      <p>
+        Esse é o meu Conversor de Arquivos pessoal - um projeto pessoal para
+        fins de aprendizado das tecnologias utilizadas no seu desenvolvimento.
+        Ao usá-lo, você está concordando com as minhas Políticas de Privacidade
+        que estão destacadas aqui. Se não concordar, melhor não usar, beleza?
+      </p>
+
+      <div className="space-y-2">
+        <h2 className="text-xl font-medium text-gray-700 md:text-2xl">
+          1. Informações coletadas
+        </h2>
+
+        <p>
+          Coletamos e usamos algumas informações para melhorar a experiência do
+          usuário. O que inclui: interação em nosso site, páginas visitadas,
+          endereço IP, tipo de navegador, tipo de dispositivo e URLs de
+          referência. Essas informações vêm do Google Analytics e nos ajudam a
+          entender como os usuários interagem com nosso site para aprimorar sua
+          funcionalidade e conteúdo.
+        </p>
+      </div>
+
+      <div className="space-y-2">
+        <h2 className="text-xl font-medium text-gray-700 md:text-2xl">
+          2. Como usaremos suas informações
+        </h2>
+
+        <p>
+          Usamos as informações coletadas através do Google Analytics apenas pra
+          entender melhor o comportamento do usuário em nosso site e dar uma
+          turbinada nele. Não vendemos, alugamos ou compartilhamos esses dados
+          com terceiros!
+        </p>
+      </div>
+
+      <div className="space-y-2">
+        <h2 className="text-xl font-medium text-gray-700 md:text-2xl">
+          3. Cookies e Tecnologias de Rastreamento
+        </h2>
+
+        <p>
+          Nós usamos cookies e tecnologias de rastreamento parecidas para
+          coletar e guardar informações sobre suas interações com nosso site.
+          Você pode ajustar isso nas configurações do seu navegador. Só
+          lembrando que, se desativar os cookies, pode ser que sua experiência
+          no nosso site seja um pouquinho diferente.
+        </p>
+      </div>
+
+      <div className="space-y-2">
+        <h2 className="text-xl font-medium text-gray-700 md:text-2xl">
+          4. Segurança dos Dados
+        </h2>
+
+        <p>
+          Fazemos o possível para proteger seus dados contra acessos não
+          autorizados, vazamentos, alterações ou destruição. Mas, vale lembrar,
+          que por mais que a gente capriche, nenhum método de transmissão de
+          dados pela internet ou armazenamento eletrônico é totalmente à prova
+          de falhas.
+        </p>
+      </div>
+
+      <div className="space-y-2">
+        <h2 className="text-xl font-medium text-gray-700 md:text-2xl">
+          5. Links para sites externos
+        </h2>
+
+        <p>
+          Nosso site pode ter links que levam para sites ou serviços de
+          terceiros, e aí é bom lembrar, esses lugares não estão sob nosso
+          comando. A gente não tem poder sobre o conteúdo, as políticas de
+          privacidade ou qualquer outro procedimento desses sites de terceiros.
+          Então, antes de compartilhar suas informações pessoais, confere a
+          política de privacidade deles.
+        </p>
+      </div>
+
+      <div className="space-y-2">
+        <h2 className="text-xl font-medium text-gray-700 md:text-2xl">
+          6. Privacidade para menores de idade
+        </h2>
+
+        <p>
+          Nosso site não foi projetado para menores de idade. Nós não coletamos,
+          de propósito, informações pessoais de menores de idade. Se você é pai,
+          mãe ou responsável e acha que seu filho(a) compartilhou alguma
+          informação pessoal com a gente, entra em contato que a gente remove os
+          dados.
+        </p>
+      </div>
+
+      <div className="space-y-2">
+        <h2 className="text-xl font-medium text-gray-700 md:text-2xl">
+          7. Atualizações na Política de Privacidade
+        </h2>
+
+        <p>
+          Nós nos reservamos ao direito de atualizar ou modificar nossa Política
+          de Privacidade sempre quando acharmos necessário, sem aviso prévio.
+          Qualquer mudança já entra em vigor assim que publicado nessa página. A
+          data da última revisão fica lá em cima, bem no início.
+        </p>
+      </div>
+
+      <div className="space-y-2">
+        <h2 className="text-xl font-medium text-gray-700 md:text-2xl">
+          8. Contato
+        </h2>
+
+        <p>
+          Caso ainda tenha dúvidas sobre nossa Política de Privacidade, entre em
+          contato via Github (topo da página).
+        </p>
+      </div>
+
+      <p>
+        Ao fazer uso desse site, você está concordando com a Política de
+        Privacidade. Se não está de acordo, melhor deixar de usar meu Conversor
+        de Arquivos pessoal. Obrigado pela confiança.
+      </p>
+
+      <p className="text-center text-sm">
+        PS: Esse projeto foi inspirado no Modifio. Thank you Souhail
+        Ben-Lhachemi for sharing this excellent work with the dev community.
+      </p>
+    </div>
+  )
 }
 
 export default PrivacyPolicy
